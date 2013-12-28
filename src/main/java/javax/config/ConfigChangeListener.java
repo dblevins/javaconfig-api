561531fa5a626309856a51d32f24c74c33e3ed1e
Anatole Tresch <atsticks@gmail.com>
2013-12-28 11:22:11 +0100

    Added Javadocs.

--- src/main/java/javax/config/ConfigChangeListener.java
+++ src/main/java/javax/config/ConfigChangeListener.java
@@ -1,18 +1,27 @@
 /*
- * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE
- * CONDITION THAT YOU ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT.
- * PLEASE READ THE TERMS AND CONDITIONS OF THIS AGREEMENT CAREFULLY. BY
- * DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF THE
- * AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE"
- * BUTTON AT THE BOTTOM OF THIS PAGE.
- * 
- * Specification: JSR-xxx Java Configuration API ("Specification")
- * 
- * Copyright (c) 2012-2013, Credit Suisse All rights reserved.
+ * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE CONDITION THAT YOU
+ * ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT. PLEASE READ THE TERMS AND CONDITIONS OF THIS
+ * AGREEMENT CAREFULLY. BY DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF
+ * THE AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE" BUTTON AT THE
+ * BOTTOM OF THIS PAGE. Specification: JSR-xxx Java Configuration API ("Specification") Copyright
+ * (c) 2012-2013, Credit Suisse All rights reserved.
  */
 package javax.config;
 
+/**
+ * Interface implemented by code interested in configuration changes, especially for code running in
+ * a standalone/non CDI context. When CDI is available, {@link ConfigChangeEvent} are distributed by
+ * sending corresponding enterprise events.
+ * 
+ * @author Anatole Tresch
+ */
 public interface ConfigChangeListener {
-
+	
+	/**
+	 * Method called on change.
+	 * 
+	 * @param event
+	 *            the {@link ConfigChangeEvent}, never {@code null}.
+	 */
 	void configChanged(ConfigChangeEvent event);
 }
